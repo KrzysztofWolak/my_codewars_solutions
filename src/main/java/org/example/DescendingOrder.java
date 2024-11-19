a37abfc658fa1cdf9717c174bfbaff2b7483a87a
Admin <crisswol@interia.pl>
2024-11-19 11:44:53 +0100

    next training commit

---
+++ src/main/java/org/example/DescendingOrder.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+//
+//Examples:
+//Input: 42145 Output: 54421
+//
+//Input: 145263 Output: 654321
+//
+//Input: 123456789 Output: 987654321
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        List<String> list = new ArrayList<>();
+        String str = String.valueOf(num);
+        for (int i = 0; i < str.length(); i++) {
+            list.add(String.valueOf(str.charAt(i)));
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        String endStr = "";
+        for (String s : list) {
+            endStr += s;
+        }
+        return Integer.parseInt(endStr);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
-import java.lang.reflect.Type;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
